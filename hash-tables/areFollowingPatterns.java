3a8fdeb1a490ad35a685963c0635dce38e15cac6
Mark Pichler <mark_pichler@yahoo.com>
2017-07-18 18:09:01 -0700

    Update areFollowingPatterns.java

--- hash-tables/areFollowingPatterns.java
+++ hash-tables/areFollowingPatterns.java
@@ -1,5 +1,9 @@
 /*
-Given an array strings, determine whether it follows the sequence given in the patterns array. In other words, there should be no i and j for which strings[i] = strings[j] and patterns[i] ≠ patterns[j] or for which strings[i] ≠ strings[j] and patterns[i] = patterns[j].
+Given an array strings, determine whether it follows the sequence given 
+in the patterns array. In other words, there should be no i and j for 
+which strings[i] = strings[j] and patterns[i] ≠ patterns[j] or for which 
+strings[i] ≠ strings[j] and patterns[i] = patterns[j].
+-CodeFights.com
 */
 
 boolean areFollowingPatterns(String[] strings, String[] patterns) {
